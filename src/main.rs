745b23db0e4bc934fbd5a038798fc606e019428b
Anudeep <76202842+anudeep652@users.noreply.github.com>
2023-05-31 22:25:00 +0530

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,6 @@ use std::env::args;
 use p_kill::{get_pid_by_port, kill_process};
 
 fn main() {
-    let port = 5173;
     let args: Vec<String> = args().collect();
 
     println!("args: {:?}", args[1]);
